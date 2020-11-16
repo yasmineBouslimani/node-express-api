222a289f26f291ff509d5cab80a51b4f50e87cb9
Yasmine <yasmine.bouslimani@gmail.com>
2020-11-16 14:48:21 +0100

    Add PUT route

--- index.js
+++ index.js
@@ -40,6 +40,29 @@ app.post('/reservations', (req, res) => {
     res.status(200).json(parkings)
 })
 
+app.put('/parkings/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parking.name = req.body.name,
+        parking.city = req.body.city,
+        parking.type = req.body.type,
+        res.status(200).json(parking)
+})
+
+app.put('/reservations/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    let reservation = reservations.find(reservation => reservation.id === id)
+    reservation.parking = req.body.parking,
+        reservation.parkingId = req.body.parkingId,
+        reservation.city = req.body.city,
+        reservation.clientName = req.body.clientName,
+        reservation.vehicle = req.body.vehicle,
+        reservation.licensePlate = req.body.licensePlate,
+        reservation.checkin = req.body.checkin,
+        reservation.checkout = req.body.checkout
+    res.status(200).json(reservation)
+})
+
 app.delete('/parkings/:id', (req, res) => {
     const id = parseInt(req.params.id)
     let parking = parkings.find(parking => parking.id === id)
